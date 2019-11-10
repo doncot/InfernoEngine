52085b83537a64ead29ec6cf90de6b1e5cde1c47
doncot <doncot@gmail.com>
2019-11-10 14:48:47 +0900

    関数にdpiを決めさせる

--- InfernoEngine/GraphicsDirectX11.cpp
+++ InfernoEngine/GraphicsDirectX11.cpp
@@ -178,17 +178,12 @@ GraphicsDirectX11::GraphicsDirectX11(HWND hWnd)
 		if (FAILED(hr))
 			throw runtime_error("IDXGISwapChain1#GetBuffer()");
 
-		// DPIの取得
-		float dpiX;
-		float dpiY;
-		d2dFactory->GetDesktopDpi(&dpiX, &dpiY);
-
 		// Direct2Dの描画先となるビットマップを作成
 		D2D1_BITMAP_PROPERTIES1 d2dProp =
 			D2D1::BitmapProperties1(
 				D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
 				D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED),
-				dpiX, dpiY);
+				0, 0);
 		hr = m_p2dDC->CreateBitmapFromDxgiSurface(
 			pDxgiSurface.Get(), &d2dProp, &m_2dTargetBitmap);
 		if (FAILED(hr))
